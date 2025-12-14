a2817f54c64ab48260e5551a355603b58a266984
ethanhrabik <ethanhrabik@gmail.com>
2025-12-14 14:50:46 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildVoiceStates
+  ]
+});
+
+client.once("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.login("YOUR_BOT_TOKEN");
